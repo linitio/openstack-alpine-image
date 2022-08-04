c29bb0c19d126724e67d88e896d442c45e0f6979
linitio <kevin@linit.io>
2022-08-04 11:43:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# openstack-alpine-image
---
+++ config/configure.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+_step_counter=0
+step() {
+	_step_counter=$(( _step_counter + 1 ))
+	printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
+}
+
+
+step 'Set up timezone'
+setup-timezone -z Europe/Zurich
+
+step 'Set up networking'
+cat > /etc/network/interfaces <<-EOF
+	iface lo inet loopback
+	iface eth0 inet dhcp
+EOF
+ln -s networking /etc/init.d/net.lo
+ln -s networking /etc/init.d/net.eth0
+
+step 'Adjust rc.conf'
+sed -Ei \
+	-e 's/^[# ](rc_depend_strict)=.*/\1=NO/' \
+	-e 's/^[# ](rc_logger)=.*/\1=YES/' \
+	-e 's/^[# ](unicode)=.*/\1=YES/' \
+	/etc/rc.conf
+	
+step 'Download cloud.cfg file'
+rm /etc/cloud/cloud.cfg
+wget -O /etc/cloud/cloud.cfg https://raw.githubusercontent.com/linitio/openstack-alpine-image/main/config/cloud.cfg
+
+step 'Enable services'
+rc-update add acpid default
+rc-update add chronyd default
+rc-update add crond default
+rc-update add net.eth0 default
+rc-update add net.lo boot
+rc-update add termencoding boot
+rc-update add sshd default
+rc-update add cloud-init default
+rc-update add cloud-init-local default
+rc-update add cloud-config default
+rc-update add cloud-final default
